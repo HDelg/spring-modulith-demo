cb3372bd136d30f8c694a157a8274ecfd6497345
HDelgado <ganhio@gmail.com>
2024-11-29 23:31:44 -0600

    First commit

---
+++ src/test/java/com/hdr/spring_modulith_course/SpringModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdr.spring_modulith_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
